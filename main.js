7e575e58d2db49f2d7114bb85033df1a4929f836
bathubba <58224914+bathubba@users.noreply.github.com>
2019-11-27 18:11:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,23 @@ function buyGoldPerClick() {
 var mainGameLoop = window.setInterval(function() {
   mineGold()
 }, 1000)
+
+var save = {
+    cookies: cookies,
+    cursors: cursors,
+    prestige: prestige
+  localStorage.setItem("save",JSON.stringify(save)); 
+}
+
+var savegame = JSON.parse(localStorage.getItem("save")); 
+
+if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies; {
+localStorage.removeItem("save")
+}
+
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+document.getElementById('cookies').innerHTML = prettify(cookies); 
